605b121fcff72bbc8cf2687c6547c0304381f55d
GCSebastian <juan.gaiborc.est@uets.edu.ec>
2024-05-10 11:04:08 -0500

    Camara
    waza

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
+  imagenParaMostrar:any="";
+  imagenTomada:any;  
   constructor() {}
-
-}
+  ngOnInit(): void {
+    Camera.requestPermissions();
+  }
+  async getPicture() {
+    this.imagenTomada = await Camera.getPhoto({
+      quality: 90,
+      resultType: CameraResultType.Uri,
+      source:CameraSource.Camera
+    });
+    if(this.imagenTomada){
+			this.imagenParaMostrar = this.imagenTomada
+    }
+}}
